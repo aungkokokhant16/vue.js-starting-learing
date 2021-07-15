351f715af2c600ace015bfa86014888e4d43765e
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2021-07-15 22:24:55 +0630

    third add

--- app.js
+++ app.js
@@ -1,30 +1,51 @@
-const app=Vue.createApp({
-    data(){
-        return{
-            title:"You don't js",
-            author:"anonymous",
-            age:"20",
-            showbook:true,
+const app = Vue.createApp({
+    data() {
+        return {
+            // title:"You don't js",
+            // author:"anonymous",
+            // age:"20",
+            books: [
+                {
+                    title: "You don't js",
+                    author: "anonymous",
+                    age: 20,
+                },
+
+                {
+                    title: "You don't js",
+                    author: "anonymous",
+                    age: 20,
+                },
+
+                {
+                    title: "You don't js",
+                    author: "anonymous",
+                    age: 20,
+                }
+            ],
+
+            showbook: true,
+            link: "http://www.google.com"
 
 
         }
     },
 
-    methods:{
-        IncreaseAge(){
-            this.age++
-        },
-        mouseHover(event){
-            console.log("mouse hover is working",event.type);
-        },
-        mouseleafH(event){
-            console.log("mose leaf is working",event.type);
-        },
-        mousedouble(event){
-            console.log("mouse douvlie working,event",event.type);
-        }
+    methods: {
+        // IncreaseAge(){
+        //     this.age++
+        // },
+        // mouseHover(event){
+        //     console.log("mouse hover is working",event.type);
+        // },
+        // mouseleafH(event){
+        //     console.log("mose leaf is working",event.type);
+        // },
+        // mousedouble(event){
+        //     console.log("mouse douvlie working,event",event.type);
+        // }
     }
-   
+
 });
 
 app.mount('#app');
